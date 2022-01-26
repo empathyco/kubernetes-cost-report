f1ba3f33a2a3f0920d46571c9010274fbcb03398
Augusto <59477015+cesarempathy@users.noreply.github.com>
2022-01-26 17:07:30 +0100

    test ci (#21)
    * add kics
    
    * add tfsec
    
    * fix kics
    
    * fix kics
    
    * test tfsec
    
    * fix
    
    * test tfsec
    
    * add diagrams
    
    * add tfsec terraform dir
    
    * fix persistance
    
    * test comments
    
    * test comments
    
    * test comments
    
    * tfsec fail
    
    * tfsec fail
    
    * tfsec fail
    
    * updagrde tfsec
    
    * updagrde tfsec
    
    * fix dockefile
    
    * improve code security
    
    * add self hosted
    
    * add ignore for policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -8,28 +8,24 @@ module "cost_report_iam_assumable_role_with_oidc" {
   role_policy_arns              = [aws_iam_policy.cost_report_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:${local.oidc.namespace}:${local.oidc.serviceaccount}"]
 }
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    sid       = ""
+    effect    = "Allow"
+    resources = ["*"]#tfsec:ignore:aws-iam-no-policy-wildcards
+    actions   = ["pricing:DescribeServices","pricing:GetAttributeValues","pricing:GetProducts"]
+  }
+
+  statement {
+    sid       = ""
+    effect    = "Allow"
+    resources = ["*"]#tfsec:ignore:aws-iam-no-policy-wildcards
+    actions   = ["ec2:DescribeInstances"]
+  }
+}
 resource "aws_iam_policy" "cost_report_policy" {
   name = "cost_report_policy"
   description = "cost_report_policy"
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "pricing:*"
-            ],
-            "Effect": "Allow",
-            "Resource": "*"
-        },
-        {
-            "Action": [
-                "ec2:Describe*"
-            ],
-            "Effect": "Allow",
-            "Resource": "*"
-        }
-    ]
-}
-EOF
+  policy = data.aws_iam_policy_document.policy.json
 }
