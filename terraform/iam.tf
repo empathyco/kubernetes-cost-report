cc49c8c7ac6bb7813bfd8ae1a8db30fc460e0d8e
cesarg <cesarg@empathy.co>
2022-01-26 15:37:36 +0100

    updagrde tfsec

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -8,29 +8,25 @@ module "cost_report_iam_assumable_role_with_oidc" {
   role_policy_arns              = [aws_iam_policy.cost_report_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:${local.oidc.namespace}:${local.oidc.serviceaccount}"]
 }
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    sid       = ""
+    effect    = "Allow"
+    resources = ["*"]
+    actions   = ["pricing:*"]
+  }
+
+  statement {
+    sid       = ""
+    effect    = "Allow"
+    resources = ["*"]
+    actions   = ["ec2:Describe*"]
+  }
+}
 resource "aws_iam_policy" "cost_report_policy" {
   name = "cost_report_policy"
   description = "cost_report_policy"
 # Test
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "pricing:*"
-            ],
-            "Effect": "Allow",
-            "Resource": "*"
-        },
-        {
-            "Action": [
-                "ec2:Describe*"
-            ],
-            "Effect": "Allow",
-            "Resource": "*"
-        }
-    ]
-}
-EOF
+  policy = data.aws_iam_policy_document.policy.json
 }
